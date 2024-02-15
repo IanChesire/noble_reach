b6e2930f3ffdb1a4b5795137f3b969877d3a3f18
Lamech74 <omwegalamech@gmail.com>
2024-02-15 15:38:48 +0300

    tailwind done

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,20 @@
-// App.js
-import React, { useState } from 'react';
-import VolunteerForm from './VolunteerForm';
-import ScamReportForm from './ScamReportForm';
+import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import VolunteerForm from './VolunteerForm'
+import ScamReportForm from './ScamReportForm'
+// import './App.css'
 
-const App = () => {
-  const [showVolunteerForm, setShowVolunteerForm] = useState(false);
-  const [showScamReportForm, setShowScamReportForm] = useState(false);
-
-  const handleVolunteerClick = () => {
-    setShowVolunteerForm(true);
-  };
-
-  const handleScamReportClick = () => {
-    setShowScamReportForm(true);
-  };
-
-  const handleCloseForm = () => {
-    setShowVolunteerForm(false);
-    setShowScamReportForm(false);
-  };
-
-  const handleVolunteerFormSubmit = (formData) => {
-    // Handle volunteer form submission (e.g., send data to the server)
-    console.log('Volunteer form submitted:', formData);
-    // Close the form
-    setShowVolunteerForm(false);
-  };
-
-  const handleScamReportFormSubmit = (formData) => {
-    // Handle scam report form submission (e.g., send data to the server)
-    console.log('Scam report submitted:', formData);
-    // Close the form
-    setShowScamReportForm(false);
-  };
+function App() {
+  
+  // All routes placed here.
 
   return (
-    <div>
-      <h1>Your Application</h1>
-      
-      <button onClick={handleVolunteerClick}>Volunteer</button>
-      <button onClick={handleScamReportClick}>Report Scam</button>
+    <>
+   <h1>Hello Team B.</h1>
+   <VolunteerForm/>
+   <ScamReportForm/> 
+    </>
+  )
+}
 
-      {showVolunteerForm && (
-        <VolunteerForm onClose={handleCloseForm} onSubmit={handleVolunteerFormSubmit} />
-      )}
-
-      {showScamReportForm && (
-        <ScamReportForm onClose={handleCloseForm} onSubmit={handleScamReportFormSubmit} />
-      )}
-    </div>
-  );
-};
-
-export default App;
+export default App
